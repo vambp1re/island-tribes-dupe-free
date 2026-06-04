37bf00cebe5fe0a0991099b2ec0a04671ba16338
vambp1re <sillly.amber@gmail.com>
2026-06-03 17:33:23 -0700

    Update Dupe.lua

--- Dupe.lua
+++ Dupe.lua
@@ -429,8 +429,8 @@ DupeButton.MouseButton1Click:Connect(function()
 end)
 
 local DragHandle = Instance.new("Frame")
-DragHandle.Size = UDim2.new(0.5, 0, 0, 8) 
-DragHandle.Position = UDim2.new(0.5, 0, 0.5, 0) 
+DragHandle.Size = UDim2.new(0.5, 0, 0, 3.5) 
+DragHandle.Position = UDim2.new(0.5, 0, 2, 0) 
 DragHandle.AnchorPoint = Vector2.new(0.5, 0.5)
 DragHandle.BackgroundColor3 = Color3.fromRGB(160, 160, 160)
 DragHandle.BackgroundTransparency = 0.5
@@ -438,9 +438,6 @@ DragHandle.BorderSizePixel = 0
 DragHandle.Parent = TopBar
 DragHandle.Active = true
 DragHandle.ZIndex = TopBar.ZIndex + 10
-
-CloseBtn.Parent = TopBar
-CloseBtn.Position = UDim2.new(1, -35, 0, 5) 
 
 local dragging = false
 local dragStart, startPos
